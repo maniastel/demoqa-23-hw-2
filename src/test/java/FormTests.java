57d0c32a5c2b33135004853644429f8c6d949fd9
Maria Stelmakh <maniastelmah@gmail.com>
2023-10-29 18:24:46 +0400

    added form filling test

---
+++ src/test/java/FormTests.java
@@ -0,0 +1,58 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class FormTests {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void fillFormTest() {
+
+        open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+        $("#firstName").setValue("Ivan");
+        $("#lastName").setValue("Ivanov");
+        $("#userEmail").setValue("iivanov@mail.com");
+        $("label[for='gender-radio-1']").click();
+        $("#userNumber").setValue("7900000000");
+
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("January");
+        $(".react-datepicker__year-select").selectOption("1995");
+        $("div[class*='react-datepicker__day--003").click();
+
+        $("#subjectsInput").setValue("Arts").pressEnter();
+        $("label[for='hobbies-checkbox-2']").click();
+        $("#uploadPicture").uploadFile(new File("src/attachment/picture.png"));
+        $("#currentAddress").setValue("12 Street, City");
+        $("#react-select-3-input").setValue("NCR").pressEnter();
+        $("#react-select-4-input").setValue("Noida").pressEnter();
+        $("#submit").click();
+
+        //form data check
+        $(".table-responsive").shouldHave(text("Ivan Ivanov"));
+        $(".table-responsive").shouldHave(text("iivanov@mail.com"));
+        $(".table-responsive").shouldHave(text("Male"));
+        $(".table-responsive").shouldHave(text("7900000000"));
+        $(".table-responsive").shouldHave(text("3 January,1995"));
+        $(".table-responsive").shouldHave(text("Arts"));
+        $(".table-responsive").shouldHave(text("Reading"));
+        $(".table-responsive").shouldHave(text("picture.png"));
+        $(".table-responsive").shouldHave(text("12 Street, City"));
+        $(".table-responsive").shouldHave(text("NCR Noida"));
+
+    }
+
+}
